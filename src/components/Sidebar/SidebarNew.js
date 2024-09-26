21f7fdba0731a739e691408ad509beff20f39d86
Qian Tang <qtang12@myseneca.ca>
2024-09-26 14:34:48 -0400

    Added Logout button to Siderbar

--- src/components/Sidebar/SidebarNew.js
+++ src/components/Sidebar/SidebarNew.js
@@ -4,68 +4,89 @@ import styles from "./SidebarNew.module.css";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 import Search from "../Search/Search";
+import {
+  RiAddLine,
+  RiSubtractLine,
+  RiLogoutCircleLine,
+  RiCircleLine,
+  RiDashboardLine,
+  RiBookReadLine,
+  RiGraduationCapLine,
+  RiUserLine,
+  RiCalendarTodoLine,
+  RiDraftLine,
+} from "@remixicon/react";
 
 function SidebarNew() {
   const icons = {
     PlusIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
-        style={{ marginRight: "0px" }}
-      >
-        <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32v144H48c-17.7 0-32 14.3-32 32s14.3 32 32 32h144v144c0 17.7 14.3 32 32 32s32-14.3 32-32V288h144c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-      </svg>
+      // <svg
+      //   xmlns="http://www.w3.org/2000/svg"
+      //   viewBox="0 0 448 512"
+      //   style={{ marginRight: "0px" }}
+      // >
+      //   <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32v144H48c-17.7 0-32 14.3-32 32s14.3 32 32 32h144v144c0 17.7 14.3 32 32 32s32-14.3 32-32V288h144c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+      // </svg>
+      <RiAddLine />
     ),
     MinusIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
-        style={{ marginRight: "0px" }}
-      >
-        <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
-      </svg>
+      // <svg
+      //   xmlns="http://www.w3.org/2000/svg"
+      //   viewBox="0 0 448 512"
+      //   style={{ marginRight: "0px" }}
+      // >
+      //   <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
+      // </svg>
+      <RiSubtractLine />
     ),
     CircleIcon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <circle
-          cx="12"
-          cy="12"
-          r="10"
-          fill="none"
-          stroke="white"
-          strokeWidth="2"
-        />
-      </svg>
+      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      //   <circle
+      //     cx="12"
+      //     cy="12"
+      //     r="10"
+      //     fill="none"
+      //     stroke="white"
+      //     strokeWidth="2"
+      //   />
+      // </svg>
+      <RiCircleLine />
     ),
     DashboardIcon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M3 13h8V3H3v10zm10 8h8V3h-8v18zm-10 0h8v-6H3v6z" />
-      </svg>
+      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      //   <path d="M3 13h8V3H3v10zm10 8h8V3h-8v18zm-10 0h8v-6H3v6z" />
+      // </svg>
+      <RiDashboardLine />
     ),
     MyCoursesIcon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M3 13h8V3H3v10zm10 8h8V3h-8v18zm-10 0h8v-6H3v6z" />
-      </svg>
+      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      //   <path d="M3 13h8V3H3v10zm10 8h8V3h-8v18zm-10 0h8v-6H3v6z" />
+      // </svg>
+      <RiCalendarTodoLine />
     ),
     StudentIcon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-      </svg>
+      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      //   <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+      // </svg>
+      <RiGraduationCapLine />
     ),
     CourseIcon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M21 3H3c-1.1 0-2 .9-2 2v14a2 2 0 002 2h18a2 2 0 002-2V5a2 2 0 00-2-2zM3 19V5h18v14H3zm4-6h2v2H7v-2zm-4 0h2v2H3v-2zm16 0h2v2h-2v-2zm-8 0h2v2h-2v-2zm4 0h2v2h-2v-2zm-8-4h2v2H7V9zm-4 0h2v2H3V9zm16 0h2v2h-2V9zm-8 0h2v2h-2V9zm4 0h2v2h-2V9z" />
-      </svg>
+      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      //   <path d="M21 3H3c-1.1 0-2 .9-2 2v14a2 2 0 002 2h18a2 2 0 002-2V5a2 2 0 00-2-2zM3 19V5h18v14H3zm4-6h2v2H7v-2zm-4 0h2v2H3v-2zm16 0h2v2h-2v-2zm-8 0h2v2h-2v-2zm4 0h2v2h-2v-2zm-8-4h2v2H7V9zm-4 0h2v2H3V9zm16 0h2v2h-2V9zm-8 0h2v2h-2V9zm4 0h2v2h-2V9z" />
+      // </svg>
+      <RiBookReadLine />
     ),
     TeacherIcon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-      </svg>
+      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      //   <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+      // </svg>
+      <RiUserLine />
     ),
     EnrollmentIcon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M12 2C6.49 2 2 6.49 2 12s4.49 10 10 10 10-4.49 10-10S17.51 2 12 2zm-1 17.93C7.06 18.69 4 15.36 4 12c0-.47.05-.92.13-1.37L9 15v1c0 1.1.9 2 2 2v1.93zM12 4c1.83 0 3.54.67 4.88 1.76L5.76 16.88C4.67 15.54 4 13.83 4 12c0-4.42 3.58-8 8-8zm1 12h-1v-4h1v4zm0-6h-1V7h1v3z" />
-      </svg>
+      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      //   <path d="M12 2C6.49 2 2 6.49 2 12s4.49 10 10 10 10-4.49 10-10S17.51 2 12 2zm-1 17.93C7.06 18.69 4 15.36 4 12c0-.47.05-.92.13-1.37L9 15v1c0 1.1.9 2 2 2v1.93zM12 4c1.83 0 3.54.67 4.88 1.76L5.76 16.88C4.67 15.54 4 13.83 4 12c0-4.42 3.58-8 8-8zm1 12h-1v-4h1v4zm0-6h-1V7h1v3z" />
+      // </svg>
+      <RiDraftLine />
     ),
   };
 
@@ -188,6 +209,11 @@ function SidebarNew() {
           </div>
         ))}
       </div>
+
+      <Link to="/" className={styles.logout}>
+        <RiLogoutCircleLine />
+        <span>Logout</span>
+      </Link>
     </div>
   );
 }
