6cc713df0d9f87520ab827424becc21b464ba564
Mia Le <65566882+tknle@users.noreply.github.com>
2024-10-11 22:06:47 -0400

    Add, Edit, View, View All added for Program, add a program form

--- src/features/Program/ProgramTable.js
+++ src/features/Program/ProgramTable.js
@@ -3,27 +3,17 @@ import generalStyles from "../../generalStyles.module.css";
 import styles from "../../components/Table.module.css";
 import { Link } from "react-router-dom";
 import Loader from "../../ui/Loader";
-import useCheckbox from "../../hooks/useCheckbox";
 
 function ProgramTable({ programData, rowsPerPage, currPage, isLoading }) {
   const currData = programData.slice(
     (currPage - 1) * rowsPerPage,
     currPage * rowsPerPage
   );
-
+  console.log("currData " + JSON.stringify(currData));
   return (
     <table className={styles.table}>
       <thead>
         <tr>
-          <th>
-            <input
-              type="checkbox"
-              checked={isAllSelected}
-              onChange={() =>
-                handleSelectAll(currData.map((element) => element.ProgramCode))
-              }
-            />
-          </th>
           <th>S/N</th>
           <th>Program Code</th>
           <th>Program Name</th>
@@ -36,13 +26,6 @@ function ProgramTable({ programData, rowsPerPage, currPage, isLoading }) {
         ) : (
           currData.map((program, index) => (
             <tr key={program.ProgramNo} className={styles.tr}>
-              <td>
-                <input
-                  type="checkbox"
-                  checked={selectedCheckboxes.includes(program.ProgramCode)}
-                  onChange={() => handleCheckboxes(program.ProgramCode)}
-                />
-              </td>
               <td>{index + 1 + (currPage - 1) * rowsPerPage}</td>
               <td>{program.ProgramCode}</td>
               <td>{program.ProgramName}</td>
