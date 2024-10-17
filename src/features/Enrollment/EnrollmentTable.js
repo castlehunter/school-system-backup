68d393c2919fd64e7018121a21bec45d3907e949
Mia Le <65566882+tknle@users.noreply.github.com>
2024-10-16 21:32:06 -0400

    add checkbox to program table and enrollment, remove enrollment action

--- src/features/Enrollment/EnrollmentTable.js
+++ src/features/Enrollment/EnrollmentTable.js
@@ -3,17 +3,35 @@ import generalStyles from "../../generalStyles.module.css";
 import styles from "../../components/Table.module.css";
 import { Link } from "react-router-dom";
 import Loader from "../../ui/Loader";
+import useCheckbox from "../../hooks/useCheckbox";
 
 function EnrollmentTable({ enrollmentData, rowsPerPage, currPage, isLoading }) {
   const currData = enrollmentData.slice(
     (currPage - 1) * rowsPerPage,
     currPage * rowsPerPage
   );
+  console.log('currData ' + JSON.stringify(currData));
+  const {
+    isAllSelected,
+    handleSelectAll,
+    selectedCheckboxes,
+    handleCheckboxes,
+  } = useCheckbox();
 
   return (
     <table className={styles.table}>
       <thead>
         <tr>
+          <th> 
+            <input
+              type="checkbox"
+              checked={isAllSelected}
+              onChange={() =>
+                handleSelectAll(currData.map((element) => element.EnrollmentID))
+              }
+              className={styles.checkbox}
+            />
+            </th>
           <th>S/N</th>
           <th>Student First Name</th>
           <th>Student Last Name</th>
@@ -29,26 +47,27 @@ function EnrollmentTable({ enrollmentData, rowsPerPage, currPage, isLoading }) {
         ) : (
           currData.map((enrollment, index) => (
             <tr key={enrollment.enrollmentId} className={styles.tr}>
+               <td>
+              <input
+                type="checkbox"
+                checked={selectedCheckboxes.includes(enrollment.EnrollmentID)}
+                onChange={() => handleCheckboxes(enrollment.EnrollmentID)}
+                className={styles.checkbox}
+              />
+              </td>
               <td>{index + 1 + (currPage - 1) * rowsPerPage}</td>
               <td>{enrollment.Students.Users.FirstName}</td>
               <td>{enrollment.Students.Users.LastName}</td>
               <td>{enrollment.Courses.CourseName}</td>
               <td>{new Date(enrollment.EnrollmentDate).toLocaleDateString()}</td>
               <td>{enrollment.isFinished ? "Yes" : "No"}</td>
-              <td>
+              <td>              
                 <Link
-                  to={`/enrollments/${enrollment.enrollmentId}`}
+                  to={`/enrollments/edit/${enrollment.EnrollmentID}`}
                   className={generalStyles.link}
                   style={{ paddingRight: 15 + 'px' }}
-                >
-                  View
-                </Link>
-                <Link
-                  to={`/enrollments/edit/${enrollment.enrollmentId}`}
-                  className={generalStyles.link}
-                  style={{ paddingRight: 15 + 'px' }}
-                >
-                  Edit
+                  >
+                  Update Status
                 </Link>
               </td>
             </tr>
