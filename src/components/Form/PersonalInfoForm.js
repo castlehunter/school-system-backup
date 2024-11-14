ed333eaca0ce2ae6c0410d41959c39ad04eb47a1
Aaron Maniyakku <aaron@mechaweka.co.nz>
2024-11-13 22:06:47 -0500

    added upload picture function

--- src/components/Form/PersonalInfoForm.js
+++ src/components/Form/PersonalInfoForm.js
@@ -1,10 +1,11 @@
 import EditContainer from "../../ui/Layout/EditContainer";
 import formStyles from "./Form.module.css";
-import avatar from "../../assets/user-avatar-account.jpg";
+import avatar from "../../assets/User-avatar-default.jpg";
 import { useState, useEffect } from "react";
 import Button from "../Button/Button";
 import { getProfileInfoByNo } from "../../services/apiUser";
 import { UpdatePersonalInfo } from "../../services/apiUser";
+import { UploadProfileImage, GetImageURL } from "../../services/apiUser";
 
 function PersonalInfoForm({ userNo }) {
   const [personalInfoData, setPersonalInfoData] = useState({
@@ -15,8 +16,10 @@ function PersonalInfoForm({ userNo }) {
     Email: "",
     HomeAddress: "",
     DateOfBirth: "",
+    AvatarURL: "",
   });
   const [isEdit, setIsEdit] = useState(false);
+  const [imageFile, setImageFile] = useState(null);
 
   useEffect(() => {
     const fetchProfileData = async () => {
@@ -31,6 +34,7 @@ function PersonalInfoForm({ userNo }) {
           Email: profileData.Email || "",
           HomeAddress: profileData.HomeAddress || "",
           DateOfBirth: profileData.DateOfBirth || "",
+          AvatarURL: profileData.AvatarURL || "",
         });
       } catch (error) {
         console.error("Error fetching profile info:", error);
@@ -76,6 +80,35 @@ function PersonalInfoForm({ userNo }) {
     }
   }
 
+  const handleFileChange = (event) => {
+    setImageFile(event.target.files[0]);
+  };
+
+  const handleImageUpload = async () => {
+    if (imageFile) {
+      const avatarUrl = await UploadProfileImage(imageFile);
+      if (avatarUrl) {
+        const url = await GetImageURL(imageFile);
+        console.log(url);
+        if (!url)
+        {
+          setPersonalInfoData({
+            ...personalInfoData,
+            AvatarURL: url,
+          });
+        } else {
+          alert("Couldn't fetch the URL");
+        }
+        
+        alert("Image uploaded successfully!");
+      } else {
+        alert("Image upload failed.");
+      }
+    } else {
+      alert("Please select an image to upload.");
+    }
+  };
+
   return (
     <EditContainer
       title="Personal Information"
@@ -87,8 +120,9 @@ function PersonalInfoForm({ userNo }) {
     >
       <div className={formStyles.sectionLayout}>
         <div className={formStyles.avatar}>
-          <img src={avatar} alt="user avatar" />
-          <Button>Upload Picture</Button>
+          <img src={personalInfoData.AvatarURL || avatar} alt="user avatar" />
+          <input type="file" accept="image/*" onChange={handleFileChange} />
+          <Button onClickBtn={handleImageUpload}>Upload Picture</Button>
         </div>
         <form>
           <div className={formStyles.formRow}>
